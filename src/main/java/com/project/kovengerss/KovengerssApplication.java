11a5e5b07918028ee04a0fc9ae815d5b89aaeb2f
jaeweon <fhfkfk789@naver.com>
2022-06-18 00:56:06 +0900

    연결 확인

---
+++ src/main/java/com/project/kovengerss/KovengerssApplication.java
@@ -0,0 +1,13 @@
+package com.project.kovengerss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KovengerssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KovengerssApplication.class, args);
+    }
+
+}
