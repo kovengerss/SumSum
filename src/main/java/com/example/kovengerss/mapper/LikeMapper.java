50da28354bd2e3dd9b23ba800212e0619040e094
KimGunWoo9595 <medneter@naver.com>
2022-06-19 22:32:55 +0900

    Mapper추가본

--- src/main/java/com/example/kovengerss/mapper/LikeMapper.java
+++ src/main/java/com/example/kovengerss/mapper/LikeMapper.java
@@ -5,7 +5,14 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface LikeMapper {
-    //좋아요 갯수
-    public Integer likeCount(LikeVO likeVO);
+    //좋아요 갯수 조회
+    public Integer select(LikeVO likeVO);
+    //좋아요 누르면 insert 좋아요 +1
+    public void insert(LikeVO likeVO);
+    //좋아요 누르면 delete 좋아요 취소 -1
+    public void delete(LikeVO likeVO);
+    //해당 좋아요 게시판 글 번호(boardnum) 조회
+    public Integer getBoardNum(LikeVO likeVO);
+
 
 }
--- src/main/java/com/example/kovengerss/mapper/TimeMapper.java
+++ src/main/java/com/example/kovengerss/mapper/TimeMapper.java
@@ -4,7 +4,7 @@ package com.example.kovengerss.mapper;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface TimeMapper {
+public interface TimeMapper {  /* 단위테스트 시험용 mapperInterface 저희 sumsum프로젝트와 관련없습니다*/
 
     public String getTime();
 }
