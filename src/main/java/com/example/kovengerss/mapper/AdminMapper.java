9f476712919bdc4dbdc80696ddc34477f718de5e
whddla <whddla8912@gmail.com>
2022-06-23 17:02:26 +0900

    관리자 로그인, 관리자 페이지 메뉴 탭 구현

--- src/main/java/com/example/kovengerss/mapper/AdminMapper.java
+++ src/main/java/com/example/kovengerss/mapper/AdminMapper.java
@@ -10,12 +10,14 @@ import org.apache.ibatis.annotations.Mapper;
 public interface AdminMapper {
     //관리자 회원가입
     public void insert(AdminVO adminVO);
+    //관리자 로그인
+    public AdminVO adminLogin(String adminId, String adminPw);
     //게시글 삭제
     public void deleteBoard(BoardVO boardVO);
     //게시글 수정
     public void updateBoard(BoardVO boardVO);
     //게시글 신고횟수 조회
-    public Integer getWarningCount(BoardVO boardVO);
+    public int getWarningCount(BoardVO boardVO);
     //회원 탈퇴
     public void deleteUser(UserVO userVO);
 }
