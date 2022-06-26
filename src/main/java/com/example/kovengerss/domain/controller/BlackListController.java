4beb096e61d96dde15ea6a59bda7736cbd3510a1
whddla <whddla8912@gmail.com>
2022-06-26 18:51:01 +0900

    관리자 목록뽑기

--- src/main/java/com/example/kovengerss/domain/controller/BlackListController.java
+++ src/main/java/com/example/kovengerss/domain/controller/BlackListController.java
@@ -13,10 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BlackListController {
     private final BlackListService blackListService;
 
-    @GetMapping("adminPage")
+    /*@GetMapping("adminPage")
     public void blackListNum(){
 
-    }
+    }*/
 
 
 }
--- src/main/java/com/example/kovengerss/domain/service/AdminServiceImpl.java
+++ src/main/java/com/example/kovengerss/domain/service/AdminServiceImpl.java
@@ -3,11 +3,14 @@ package com.example.kovengerss.domain.service;
 import com.example.kovengerss.domain.dao.AdminDAO;
 import com.example.kovengerss.domain.vo.AdminVO;
 import com.example.kovengerss.domain.vo.BoardVO;
+import com.example.kovengerss.domain.vo.Criteria;
 import com.example.kovengerss.domain.vo.UserVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,32 +19,32 @@ public class AdminServiceImpl implements AdminService{
     private final AdminDAO adminDAO;
 
     @Override
-    public void adminInsert(AdminVO adminVO) {
-
-    }
-
-    @Override
     public AdminVO adminLogin(String adminId, String adminPw){
         return adminDAO.adminLogin(adminId, adminPw);
     }
 
     @Override
-    public void adminDeleteBoard(BoardVO boardVO) {
-
+    public boolean remove(int boardNum) {
+        return adminDAO.remove(boardNum);
     }
 
     @Override
-    public void adminUpdateBoard(BoardVO boardVO) {
-
+    public String getUserId(int userNum){
+        return adminDAO.getUserId(userNum);
     }
 
     @Override
-    public void adminGetWarningCount(BoardVO boardVO) {
-
+    public List<BoardVO> getList(Criteria criteria) {
+        return adminDAO.getList(criteria);
     }
 
     @Override
-    public void adminDeleteUser(UserVO userVO) {
+    public int getCount(int boardNum) {
+        return adminDAO.getCount(boardNum);
+    }
 
+    @Override
+    public boolean deleteUser(int userNum) {
+        return adminDAO.deleteUser(userNum);
     }
 }
